d7a2294b4165806962a8494da43b63a3059445ac
Oscar Gandia Ubeda <og.ubeda@gmail.com>
2020-05-21 13:02:34 +0200

    Delete menu.php

--- frameworkCars.v.1/view/inc/menu.php
+++
@@ -1,9 +0,0 @@
-<div id="menu">
-	<ul>
-		<li><a href = "index.php" data-tr = "Homepage"></a></li>
-		<li><a href = "index.php?page=controllerCars&op=list" data-tr = "Our Cars"></a></li>
-		<li><a href = "index.php?page=services" data-tr = "Services"></a></li>
-		<li><a href = "index.php?page=aboutus" data-tr = "About Us"></a></li>
-		<li><a href = "index.php?page=contactus" data-tr = "Contact Us"></a></li>
-	</ul>
-</div>
